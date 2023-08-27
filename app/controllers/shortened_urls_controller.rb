5b0aab934c3d91a1316975e513c54581596f4e62
hokageCV <chaitanyavaru@gmail.com>
2023-08-27 20:51:13 +0530

    feat: redirect to original url

--- app/controllers/shortened_urls_controller.rb
+++ app/controllers/shortened_urls_controller.rb
@@ -1,2 +1,12 @@
 class ShortenedUrlsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :not_found
+    def redirect_to_original
+        shortened_url_data = ShortenedUrl.find_by(shortened_code: params[:short_code])
+        
+        if shortened_url_data
+          redirect_to shortened_url_data.original_url, allow_other_host: true
+        else
+          redirect_to '/404'
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   post '/create-shortened-url', to: 'home#create_shortened_url', as: 'create_shortened_url'
 
+  get '/:short_code', to: 'shortened_urls#redirect_to_original'
+  get '/404', to: 'errors#not_found'
 
   root 'home#index'
 end
